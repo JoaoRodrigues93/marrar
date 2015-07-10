026f266ea8453fdbb0146766c05d8d70a33e6606
JoaoRodrigues93 <rodrigueslcn@hotmail.com>
2015-07-10 11:10:29 +0200

    Configuração da base de dados - alterei os dados do .env no directório raiz e modifiquei os seguintes dados para a conexão com a base de dados:
    DB_HOST=localhost
    DB_DATABASE=marrar
    DB_USERNAME=root
    DB_PASSWORD=

---
+++ database/migrations/2015_07_09_123922_create_perguntas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerguntasTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('perguntas', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('questao');
+            $table->string('opcao1');
+            $table->string('opcao2');
+            $table->string('opcao3');
+            $table->string('opcao4');
+            $table->string('opcao5');
+            $table->integer('tema_id')->unsigned();
+            $table->string('opcaoCorrecta');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('perguntas');
+	}
+
+}
