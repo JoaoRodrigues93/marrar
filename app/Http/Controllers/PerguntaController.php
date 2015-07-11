c23733c0779ef1849594d59c5871e6fb19b8be7c
Manuel João de Azevedo <manuazevedo2@gmail.com>
2015-07-11 11:07:04 +0200

    preparando o motor de perguntas -> metodos abstractos

--- app/Http/Controllers/PerguntaController.php
+++ app/Http/Controllers/PerguntaController.php
@@ -8,7 +8,8 @@ use App\Disciplina;
  */
 
 
-class PerguntaController extends Controller {
+class PerguntaController extends Controller
+{
 
 
     public function Registarpergunta()
@@ -16,11 +17,35 @@ class PerguntaController extends Controller {
 
     }
 
-public function InicializaPergunta(){
-    $disciplinas = Disciplina::lists('nome', 'id');
-    return view('pergunta')->with('disciplinas',$disciplinas);
+    public function InicializaPergunta()
+    {
+        $disciplinas = Disciplina::lists('nome', 'id');
+        return view('pergunta')->with('disciplinas', $disciplinas);
+    }
+
+
+
+
+
+
+
+
+    /*motor de peruntas começa aqui
+    aqui se encontraram os metodos que serão chamados para devoler as perguntas
+    tanto para o exame, como para testes assim como para exercicios
+    */
+    public function buscarExame($disciplina){
+        //metodo que retorna o array de perguntas do exame baseando se no unico paramentro que é a disciplina
+    }
+
+    public function buscarTeste($disciplina, $capitulo){
+        //metodo que retorna o array de perguntas para o teste baseando se no capitulo e na disciplina
+    }
+
+    public function buscarExercicios($disciplina, $capitulo, $tema){
+       //metodo que retorna o array de perguntas exercicios baseando se na discipina,capitulo e tema
+    }
+
 }
 
 
-
-}
