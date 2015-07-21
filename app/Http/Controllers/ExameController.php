0cbc241e8b6481fa94916cea2f80944cf91dfaad
JoaoRodrigues93 <rodrigueslcn@hotmail.com>
2015-07-21 20:31:01 +0200

    Novos progressos

--- app/Http/Controllers/ExameController.php
+++ app/Http/Controllers/ExameController.php
@@ -2,6 +2,10 @@
 
 use App\Disciplina;
 use App\Http\Controllers\PerguntaController;
+use Illuminate\Http\Request;
+
+
+session_start();
 
 class ExameController extends Controller
 {
@@ -10,12 +14,50 @@ class ExameController extends Controller
     {
         $perguntaController = new PerguntaController();
         $perguntas = $perguntaController->buscarExame($disciplina);
+        $nrPerguntas = $perguntas->count();
+        $_SESSION["exame"] = $perguntas;
 
-
-            return View('exame')->with(array("perguntas" => $perguntas,"disciplina"=>$disciplina));
+            return View('exame')->with(array("perguntas" => $perguntas,"disciplina"=>$disciplina,"nrPerguntas"=>$nrPerguntas));
         }
 
     public function show(){
-        return $this->showExame("Matematica");
+        return $this->showExame("História");
     }
+
+    public function corrigeExame(Request $request){
+        $exame = $_SESSION['exame'];
+        $pergunta = $exame[0];
+        $repostaEscolhida="";
+        $idPergunta=0;
+        $respostaCorrecta="";
+        $nrRepostasCertas=0;
+        $nrRepostasErradas =0;
+        $nrPerguntaActual=1;
+        $relatorio="";
+
+        foreach ($exame as $pergunta){
+            $idPergunta = $request->input("pergunta".$nrPerguntaActual);
+            $repostaEscolhida = $request->input("resposta".$nrPerguntaActual);
+            $respostaCorrecta = $pergunta->opcaoCorrecta;
+
+            if($repostaEscolhida==$respostaCorrecta){
+                $relatorio.="ID da pergunta: ".$idPergunta."  Resposta Escolhida: ".$repostaEscolhida.
+                    "  Resposta Correcta: ".$respostaCorrecta." ***RESPONDEU CORRECTAMENTE***\n";
+                $nrRepostasCertas++;
+            }
+
+            else{
+                    $relatorio.="ID da pergunta: ".$idPergunta."  Resposta Escolhida: ".$repostaEscolhida.
+                        "  Resposta Correcta: ".$respostaCorrecta." ***RESPONDEU ERRADAMENTE***\n";
+                    $nrRepostasErradas++;
+            }
+
+            $nrPerguntaActual++;
+        }
+
+
+
+        return $relatorio.$pergunta->questao;
+    }
+
 }
--- resources/views/exame.blade.php
+++ resources/views/exame.blade.php
@@ -5,13 +5,18 @@
 @section('body')
 
 <script>
-    function escolheOpcao(id,nr){
+    var nrPerguntas = parseInt('{{$nrPerguntas}}');
+
+    function escolheOpcao(id,nr,respota){
         deSeleciona(nr);
         document.getElementById('opcao'+id).setAttribute('class','bg-success');
         var btn = document.getElementById('nav'+nr);
+        var respostaEscolhida = document.getElementById("resposta"+nr);
+        respostaEscolhida.value =respota;
         btn.style.color ="#fff";
         btn.style.backgroundColor="#5cb85c";
         btn.style.borderColor="#4cae4c";
+
 
     }
     function deSeleciona(nr){
@@ -27,46 +32,48 @@
             <h2 class="text-danger">Exame | {{$disciplina}}</h2>
         </div>
         <div class="exame-time">
-                <h4 id="timer" onload="startTime()" class="text-right text-danger"></h4>
+                <h4 id="timer"  class="text-right text-danger"></h4>
         </div>
-        {!!Form::open(array('url' => 'exame')) !!}
+
        <div class="tab-content">
 
          <?php
-            $i=0;
+            $i=0; $idPerguntas="";
             foreach($perguntas as $pergunta) {
+           $idPerguntas [$i] = $pergunta->id;
             $i++;
+
             ?>
              <div id="pergunta{{$i}}" class="tab-pane fade <?php if($i==1) echo "in active"; ?>">
             <h2>{{$pergunta->questao}}</h2>
              <div class="container">
                 <div id="opcao1{{$i}}" class="">
                 <p class="text-left">
-                 <input type="radio" id="resposta1{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('1{{$i}}','{{$i}}')"/>
-                 <label for="resposta1{{$i}}">{{$pergunta->opcao1}}</label>
+                 <input type="radio" id="resposta1{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('1{{$i}}','{{$i}}','{{$pergunta->opcao1}}')"/>
+                 <label for="resposta1{{$i}}"  >{{$pergunta->opcao1}}</label>
                 </div>
 
                 <div id="opcao2{{$i}}" >
                     <p class="text-left">
-                        <input type="radio" id="resposta2{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('2{{$i}}','{{$i}}')" />
+                        <input type="radio" id="resposta2{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('2{{$i}}','{{$i}}','{{$pergunta->opcao2}}')" />
                         <label for="resposta2{{$i}}">{{$pergunta->opcao2}}</label>
                 </div>
 
                 <div id="opcao3{{$i}}">
                     <p class="text-left">
-                        <input type="radio" id="resposta3{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('3{{$i}}','{{$i}}')" />
+                        <input type="radio" id="resposta3{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('3{{$i}}','{{$i}}','{{$pergunta->opcao3}}')" />
                         <label for="resposta3{{$i}}">{{$pergunta->opcao3}}</label>
                 </div>
 
                 <div id="opcao4{{$i}}">
                     <p class="text-left">
-                        <input type="radio" id="resposta4{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('4{{$i}}','{{$i}}')" />
+                        <input type="radio" id="resposta4{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('4{{$i}}','{{$i}}','{{$pergunta->opcao4}}')" />
                         <label for="resposta4{{$i}}">{{$pergunta->opcao4}}</label>
                 </div>
 
                 <div id="opcao5{{$i}}">
                     <p class="text-left">
-                        <input type="radio" id="resposta5{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('5{{$i}}','{{$i}}')" />
+                        <input type="radio" id="resposta5{{$i}}" name="resposta{{$i}}" onclick="escolheOpcao('5{{$i}}','{{$i}}','{{$pergunta->opcao5}}')" />
                         <label for="resposta5{{$i}}">{{$pergunta->opcao5}}</label>
                     </p>
                 </div>
@@ -74,29 +81,101 @@
             </div>
             <?php } ?>
             </div>
+        {!!Form::open(array('url' => 'exame','onsubmit'=>'return valido()')) !!}
                 <ul class="nav nav-pills">
                     <?php for($j=1; $j<=$i; $j++) {
                     ?>
                             <li id="nav{{$j}}"  class="<?php if($j==1) echo "active"; ?>" ><a  data-toggle="pill" href="#pergunta{{$j}}">{{$j}}</a></li>
                     <?php }?>
+                        <?php
+                        $k=0;
+                        for($j=1; $j<=$i; $j++) {
+                            ?>
+                        <input type="hidden" id="pergunta{{$j}}" name="pergunta{{$j}}" value="<?php echo $idPerguntas[$k]; ?>" >
+                        <input type="hidden" id="resposta{{$j}}" name="resposta{{$j}}" value="" >
+                    <?php
+                        $k++;
+                        }?>
                 </ul>
           <div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 col-lg-offset-10 col-sm-offset-9 col-md-offset-10 col-xs-offset-8">
-         {!!Form::submit('Entregar',['class'=>'btn btn-primary']) !!}
+         {!!Form::submit('Entregar',['class'=>'btn btn-primary', 'id'=>'entregar']) !!}
         </div>
         {!!Form::close()!!}
     </div>
-    <script>
-        function startTimer(duration, display) {
-            var start = Date.now(),
-                    diff,
-                    minutes,
-                    seconds;
-            function timer() {
-                // get the number of seconds that have elapsed since
-                // startTimer() was called
-                diff = duration - (((Date.now() - start) / 1000) | 0);
+<div class="modal fade" id="mensagem" role="dialog">
+    <div class="modal-dialog">
 
-                // does the same job as parseInt truncates the float
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 id="modal-title" class="modal-title">Modal Header</h4>
+            </div>
+            <div id="modal-body" class="modal-body">
+                <p>Some text in the modal.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+    <script>
+        var confirmacao;
+
+        function valido () {
+            var valido = true;
+            var perguntaActualValida=true;
+            var respostaEscolhidaActual;
+            var valorEscolhido;
+            var mensagem;
+            for(i=1; i<=nrPerguntas; i++){
+                respostaEscolhidaActual = document.getElementById("resposta"+i);
+                valorEscolhido = respostaEscolhidaActual.value;
+
+                if(valorEscolhido =="" || valorEscolhido==undefined){
+                    perguntaActualValida = false;
+                    valido = false;
+                }
+            }
+
+
+            if(valido==false && confirmacao!=true){
+
+                mensagem = "<p>Deseja realmente enviar os dados do exames sem respondes todas questões?</p>"
+                +"<button name='confirmar' class='btn btn-success' id='confirmarEnvio' onclick='confirmarEnvio()' >Confirmar</button>"
+                +"    <button name='cancelar' class='btn btn-danger' id='cancelarEnvio' onclick='cancelarEnvio()'>Cancelar</button>";
+                modalAlert("Exame",mensagem);
+
+                xmlhttp = new XMLHttpRequest();
+            }
+
+            if (confirmacao==true || valido==true)
+            return true;
+            else
+            return false;
+        }
+
+        function confirmarEnvio () {
+            confirmacao = true;
+            var btn = document.getElementById('entregar');
+            btn.click();
+            closeModal();
+        }
+
+        function cancelarEnvio(){
+            closeModal();
+            confirmacao = false;
+        }
+
+        function startTimer (duration, display){
+            var start=Date.now(),diff=0,minutes, seconds,myInterval;
+
+            myInterval= setInterval(function(){timer()},1000);
+            function timer () {
+                diff = duration - (((Date.now()-start)/1000) | 0);
+
                 minutes = (diff / 60) | 0;
                 seconds = (diff % 60) | 0;
 
@@ -105,21 +184,46 @@
 
                 display.textContent = minutes + ":" + seconds;
 
-                if (diff <= 0) {
-                    // add one second so that the count down starts at the full duration
-                    // example 05:00 not 04:59
-                    start = Date.now() + 1000;
+                if(diff==30){
+                    modalAlert("Tempo de execução de Exame","O exame termina em 30 segundos.");
+                }
+
+                if(diff==0){
+                   clearInterval(myInterval);
+                    modalAlert("Tempo de execução de Exame","O tempo de execução do exame terminou");
+                    confirmacao =true;
+                    closeModal();
+                    var btn = document.getElementById('entregar');
+                    btn.click();
                 }
             };
-            // we don't want to wait a full second before the timer starts
             timer();
-            setInterval(timer, 1000);
+
+
         }
 
         window.onload = function () {
-            var fiveMinutes = 60 * 60,
-                    display = document.querySelector('#timer');
-            startTimer(fiveMinutes, display);
+            var Minutes = 60* 1, display = document.querySelector('#timer');
+            startTimer(Minutes, display);
         };
+
+
+        function modalAlert(title,message) {
+            var titleDiv, messageDiv;
+            titleDiv = document.getElementById("modal-title");
+            messageDiv = document.getElementById("modal-body")
+            titleDiv.innerHTML = ""+title;
+            messageDiv.innerHTML = "<p>"+message+"</p>";
+            $("#mensagem").modal();
+        }
+
+        function closeModal () {
+            $("#mensagem").modal("hide");
+        }
+
+        function terminarExame(){
+            var xmlhttp = new XMLHttpRequest();
+
+        }
     </script>
 @stop
