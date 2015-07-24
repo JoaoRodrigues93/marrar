a1ea1a126fc0141f6d391ec58b0b68aed4d668e1
Xavier Ngomana <xavierngomane@gmail.com>
2015-07-24 10:38:52 +0200

    Ngomana

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -89,12 +89,12 @@ Route::get('teste-validacao','TesteController@validaTeste');
 Route::get('capitulo-combobox/{id}','CapituloController@buscarCapituloDisciplina');
 Route::get('tema-combobox/{id}','TemaController@buscarTemaCapitulo');
 
-Route::get('capituloHome',function(){
-
-    return view('capituloHome');
-
-});
+Route::get('capituloHome','CapituloController@showAll');
 
 Route::get('editar_inicial', 'WelcomeController@editar_inicial');
 
 Route::get('disciplinaHome','DisciplinaController@showDisciplinaHome');
+
+
+
+Route::get('capitulo-validacao',"CapituloController@capituloTemaJason");
