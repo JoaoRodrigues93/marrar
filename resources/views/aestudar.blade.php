b273a6aba12056e178863fe3d0d6627bafc4f22a
Nelson Alexandrino <munguambe@outlook.com>
2015-09-14 16:52:13 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	public/teoria/1/1/FunÃ§Ãµes Trigonometricas.html
    	resources/views/layouts/maincontent.blade.php

--- resources/views/aestudar.blade.php
+++ resources/views/aestudar.blade.php
@@ -5,7 +5,6 @@
     <title>a marrar...</title>
     <link href="{{URL::asset('css/bootstrap.css')}} " rel="stylesheet">
     <link href="{{asset('css/marrar.css')}}" rel="stylesheet">
-    <link href="{{asset('css/w3.css')}}" rel="stylesheet">
     <script src="{{URL::asset('js/jquery.min.js')}}"></script>
     <script src="{{URL::asset('js/bootstrap.js')}} "></script>
 </head>
@@ -13,6 +12,8 @@
 
 
 <div class="container bordaCircular sombra aestudar" style="margin-top: 20px">
+
+
 
 </div>
 
