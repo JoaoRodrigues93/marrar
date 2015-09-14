f4a1bb7c4df9670112dadfc3d3a9f8d25b8308d8
Nelson Alexandrino <munguambe@outlook.com>
2015-09-14 15:40:19 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	public/teoria/1/1/FunÃ§Ãµes Trigonometricas.html
    	resources/views/layouts/maincontent.blade.php

--- resources/views/aestudar.blade.php
+++ resources/views/aestudar.blade.php
@@ -1,19 +1,119 @@
-@extends('layouts.main_a_marrar')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>a marrar...</title>
+    <link href="{{URL::asset('css/bootstrap.css')}} " rel="stylesheet">
+    <link href="{{asset('css/marrar.css')}}" rel="stylesheet">
+    <link href="{{asset('css/w3.css')}}" rel="stylesheet">
+    <script src="{{URL::asset('js/jquery.min.js')}}"></script>
+    <script src="{{URL::asset('js/bootstrap.js')}} "></script>
+</head>
+<body>
 
-@section('title')
-    Marrar | A estudar
-@stop
+<div style="background: #F1F1F1">
 
-@section('conteudo')
+    <div class="card w3-card-2">
 
-    <div class="container">
-        <div class="col-lg-6 texto">
+        {{--<b>Aprenda </b>
+        Matem&#225;tica, Portugu&#234;s,
+        F&#237;sica, Qu&#237;mica, Biologia,
+        Geografia, Hist&#243;ria, Ingl&#234;s,
+        Franc&#234;s e prepare-se
+        para seu exame de admiss&atilde;o
+        <b>gratuitamente</b>...onde e quando quiser.</p>
 
-        </div>
+        <b>Aprenda </b>
+        Matem&#225;tica, Portugu&#234;s,
+        F&#237;sica, Qu&#237;mica, Biologia,
+        Geografia, Hist&#243;ria, Ingl&#234;s,
+        Franc&#234;s e prepare-se
+        para seu exame de admiss&atilde;o
+        <b>gratuitamente</b>...onde e quando quiser.</p>
 
-        <div class="col-lg-6 exercicio">
+        mais escuro--}}
 
-        </div>
     </div>
 
-@stop
+</div>
+<div style="background: #F5F5F5">
+
+    <div class="card w3-card-2">
+        {{--<b>Aprenda </b>
+        Matem&#225;tica, Portugu&#234;s,
+        F&#237;sica, Qu&#237;mica, Biologia,
+        Geografia, Hist&#243;ria, Ingl&#234;s,
+        Franc&#234;s e prepare-se
+        para seu exame de admiss&atilde;o
+        <b>gratuitamente</b>...onde e quando quiser.</p>
+
+        <b>Aprenda </b>
+        Matem&#225;tica, Portugu&#234;s,
+        F&#237;sica, Qu&#237;mica, Biologia,
+        Geografia, Hist&#243;ria, Ingl&#234;s,
+        Franc&#234;s e prepare-se
+        para seu exame de admiss&atilde;o
+        <b>gratuitamente</b>...onde e quando quiser.</p>--}}
+
+
+
+    </div>
+
+</div>
+<div style="background: #FAFAFA">
+
+    <div class="card w3-card-2">
+
+       {{-- <b>Aprenda </b>
+        Matem&#225;tica, Portugu&#234;s,
+        F&#237;sica, Qu&#237;mica, Biologia,
+        Geografia, Hist&#243;ria, Ingl&#234;s,
+        Franc&#234;s e prepare-se
+        para seu exame de admiss&atilde;o
+        <b>gratuitamente</b>...onde e quando quiser.</p>
+
+        <b>Aprenda </b>
+        Matem&#225;tica, Portugu&#234;s,
+        F&#237;sica, Qu&#237;mica, Biologia,
+        Geografia, Hist&#243;ria, Ingl&#234;s,
+        Franc&#234;s e prepare-se
+        para seu exame de admiss&atilde;o
+        <b>gratuitamente</b>...onde e quando quiser.</p>--}}
+
+    </div>
+
+</div>
+
+<style>
+    div {
+        width: calc(100%/3);
+        height: 400px;
+
+        margin-top: 50px;
+        background: red;
+        float: right;
+    }
+
+    div > div{
+        width: 50%;
+        height: 50%;
+        background: white;
+        float: none;
+        vertical-align: middle;
+        margin: 25% 0 0 25%;
+    }
+
+    .card {
+        position: relative;
+        margin-bottom: .75rem;
+        background-color: #fff;
+        border: .0625rem solid #e5e5e5;
+        border-radius: .25rem;
+
+        overflow: scroll;
+        overflow-x: hidden;
+    }
+</style>
+
+</body>
+</html>
