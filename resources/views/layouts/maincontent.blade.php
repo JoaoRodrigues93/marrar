0907930af9aff55830729c85598a0c1af4ecdb26
Nelson Alexandrino <munguambe@outlook.com>
2015-10-13 17:26:04 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	public/teoria/1/1/FunÃ§Ãµes Trigonometricas.html
    	resources/views/layouts/maincontent.blade.php

--- resources/views/layouts/maincontent.blade.php
+++ resources/views/layouts/maincontent.blade.php
@@ -38,7 +38,7 @@
                 </a>--}}
                <div class="voltar">
                 <a id="voltar" class="voltar-a navbar-brand" href="/home">
-                          <svg fill="#FFFFFF" height="30" viewBox="0 0 22 22" width="30">
+                          <svg fill="#FFFFFF" height="30" viewBox="0 0 24 24" width="30">
                               <path d="M0 0h24v24H0z" fill="none"/>
                               <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
                           </svg>
