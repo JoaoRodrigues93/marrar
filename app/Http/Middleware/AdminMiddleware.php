687e90ab123eb55dbc301311a2dcf343823bc32c
Yola <yoyogarife@gmail.com>
2015-10-23 19:05:19 +0200

    final

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,29 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use App\Estudante;
+use Auth;
+
+class AdminMiddleware {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+
+
+        if ($request->user()->type != 'A')
+        {
+            return redirect('home');
+        }
+
+
+		return $next($request);
+	}
+
+}
